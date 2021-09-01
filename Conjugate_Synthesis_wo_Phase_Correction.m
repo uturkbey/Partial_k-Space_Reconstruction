db1acce1c9be81c7b1be4645d6b90dd05e267459
unknown <turkbey.utku@gmail.com>
2021-09-01 16:27:56 +0300

    minor change in function name

--- Conjugate_Synthesis_wo_Phase_Correction.m
+++ Conjugate_Synthesis_wo_Phase_Correction.m
@@ -1,4 +1,4 @@
-function Conjugate_Synthesis_with_Phase_Correction(im, ratio)
+function Conjugate_Synthesis_wo_Phase_Correction(im, ratio)
     %Print initial data
     subplot(3,3,1), imshow(abs(im)), title("Initial: Image Space") ;
     subplot(3,3,4), imshow(fft2c(im)), title("Initial: k-Space");
--- trial.m
+++ trial.m
@@ -1,9 +1,11 @@
 load("brain.mat")
 
-im_k_space = fft2c(im);
-[m, n] = size(im_k_space); 
-im_partial_k_space = zeros(m, n);
-im_partial_k_space(1:m, 1:n*0.5) = im_k_space(1:m, 1:n*0.5);  
-figure(7), imshow(im_partial_k_space), hold;
-im_recons = ifft2c(im_partial_k_space);
-figure(8), imshow(abs(im_recons)), hold;
+% im_k_space = fft2c(im);
+% [m, n] = size(im_k_space); 
+% im_partial_k_space = zeros(m, n);
+% im_partial_k_space(1:m, 1:n*0.5) = im_k_space(1:m, 1:n*0.5);  
+% figure(7), imshow(im_partial_k_space), hold;
+% im_recons = ifft2c(im_partial_k_space);
+% figure(8), imshow(abs(im_recons)), hold;
+
+Conjugate_Synthesis_wo_Phase_Correction(im,0.51)
